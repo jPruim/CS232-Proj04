747022ce20606eaf0ec09775a0806362ba721d3e
jPruim <44886135+jPruim@users.noreply.github.com>
2019-04-23 20:10:20 -0400

    Update dandjTester.h

--- dandjTester.h
+++ dandjTester.h
@@ -1,14 +1,21 @@
-/* test-cs232.c tests the cs232() system call.
- * Prof. Adams, for the Kernel Project,
- * CS 232 at Calvin College, March 2019.
+/* dandjTester.c tests the dandjprint, dandjfill, dandjfill2 system calls.
+ * Drew Smits and Jason Pruim, for the Kernel Project,
+ * CS 232 at Calvin College, April 2019.
  */
 
 #include <stdio.h>
-#include <DANDJPRINT.h>
+#include <dandjprint.h>
+#include <dandjfill.h>
+
 
 int main(){
 	printf("Invoking print system call...\n");
-	long result = DANDJPRINT();
+	long result = dandjprint();
+	printf("Print system call returned %ld\n", result);
+	printf("Invoking fill system call...\n");
+	char myName[20];
+	long result = dandjfill(myName);
+	printf(myName);
 	printf("Print system call returned %ld\n", result);
 	return 0;
 }
