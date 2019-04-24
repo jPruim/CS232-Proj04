2c9fc30df7d437e8b222c5e773a98899082eacb8
jPruim <44886135+jPruim@users.noreply.github.com>
2019-04-24 18:36:21 -0400

    Update sys.c

--- sys.c
+++ sys.c
@@ -2,9 +2,11 @@ SYSCALL_DEFINE0(dandjprint){
 	printk(KERN_INFO "dandj");
 	return 0;
 }
-SYSCALL_DEFINE1(dandjfill, char *, name){
-	int numElem = sizeof(name)/sizeof(name);
-	if (numElem <5){return -EFAULT;}
-	name = "dandj";
+SYSCALL_DEFINE2(dandjfill, char *, name, int, n){
+	if(n<5) return -EFAULT;
+	char buf[256];
+	long copied = strncpy_from_user(buf, name, sizeof(buf));
+	if(copied < 0 ||copied == sizeof(name)) return -EFAULT;
+	printk(KERN_INFO "dandjfill passed back: \"%s\"\n",buf);
 	return 0;
 }
