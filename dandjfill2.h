0543f7d20689ca7f07b5691dbb413a7a25da7a5d
jPruim <44886135+jPruim@users.noreply.github.com>
2019-04-24 20:19:12 -0400

    Update dandjfill2.h

--- dandjfill2.h
+++ dandjfill2.h
@@ -8,10 +8,19 @@
 
 #include <unistd.h>
 #include <sys/syscall.h>
-
+#include <sys/types.h>
+#include <pwd.h>
 #define SYS_DANDJFILL2 335
 
 static inline
 long dandjfill2(char* buf, int n) {
-	return syscall(SYS_DANDJFILL,buf, n);
+	kuid_t* uid = 0; long retRes;struct passwd *pws;char user[32];int i;
+	retRes = syscall(SYS_DANDJFILL,uid);
+	pws = getpwuid(&uid);
+	if(pws == NULL) return -1;
+	user = pws->pw_name;
+	for(i = 0;i<n&&i<32;i++){
+		buf[i]=user[i];
+	}
+	return retRes;
 }
