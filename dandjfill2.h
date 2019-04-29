b62dbe28e5e89ba2de599a1d25026554a016dec6
jPruim <44886135+jPruim@users.noreply.github.com>
2019-04-29 16:18:50 -0400

    Update dandjfill2.h

--- dandjfill2.h
+++ dandjfill2.h
@@ -9,16 +9,21 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
+#include <linux/kernel.h>
 #include <pwd.h>
 #define SYS_DANDJFILL2 335
 
 static inline
 long dandjfill2(char* buf, int n) {
-	kuid_t* uid = 0; long retRes;struct passwd *pws;char user[32];int i;
-	retRes = syscall(SYS_DANDJFILL,uid);
-	pws = getpwuid(&uid);
-	if(pws == NULL) return -1;
-	user = pws->pw_name;
+	uid_t uid[1] = {100}; long retRes;struct passwd *pws;char user[32];int i;
+	retRes = syscall(SYS_DANDJFILL2,uid);
+	pws = getpwuid(uid[0]);
+	if(pws == NULL) return -2;
+	printf("pws exists\n");
+	if(pws->pw_name == "mradmin") printf("worked\n");
+	for(i=0;i<32;i++){
+		user[i]=pws->pw_name[i];
+	}
 	for(i = 0;i<n&&i<32;i++){
 		buf[i]=user[i];
 	}
